9dc47503b0d86c56ce1b248a614112d6189d0c25
nakarinnat <nakarinnat1993@gmail.com>
2020-01-19 21:55:50 +0700

    Post method

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     //
+    protected $fillable = ['title', 'description', 'content', 'image'];
+
+    public function deleteImage()
+    {
+        Storage::delete($this->image);
+    }
 }
